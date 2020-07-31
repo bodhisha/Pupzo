c18a96cf0b477c941a72c1bdc1c473e8967a139e
bodhisha <bodhisha@gmail.com>
2020-08-01 03:18:09 +0530

    show posts on userprofile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
+  # GET user/id/show
   def show
+    @user = User.find(params[:id])
+
+    @posts = @user.posts.order(created_at: :desc)
   end
 
   def edit
