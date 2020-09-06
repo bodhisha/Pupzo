3e0af6bda7e6c3bbcd20cb12c917a5e532463423
bodhisha <bodhisha@gmail.com>
2020-09-07 00:57:50 +0530

    avoid multiple likes by same user

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,8 +2,16 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @post = Post.find(params[:post_id])
-    @post.likes.create!(user_id: current_user.id)
-    redirect_to root_path
+    if !already_liked?
+      @post = Post.find(params[:post_id])
+      @post.likes.create!(user_id: current_user.id)
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
   end
 end
