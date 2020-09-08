08f51c3471f9959f3124f2e6546ad9ba76698f59
bodhisha <bodhisha@gmail.com>
2020-09-09 04:38:47 +0530

    Redesign from scratch

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#fff4f1",
+          100: "#ffe8e2",
+          200: "#ffddd4",
+          300: "#ffc7b7",
+          400: "#ffb09a",
+          500: "#ff997d",
+          600: "#ff8e6e",
+          700: "#e68063",
+          800: "#cc7258",
+          900: "#b3634d",
+        },
+        gray: {
+          100: "#FBFAFC",
+          200: "#F7F5FA",
+          300: "#F1EDF7",
+          400: "#DFDAE8",
+          500: "#BFB8CC",
+          600: "#9187A1",
+          700: "#7D728F",
+          800: "#6A5F7A",
+          900: "#453C52",
+        },
+      },
+      fontSize: {
+        tiny: ".625rem",
+      },
+    },
+  },
+};
